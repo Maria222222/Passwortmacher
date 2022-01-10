8dcc49ce68d62d6c7c5d103ee187f22f6ae0b952
Maria222222 <97455672+Maria222222@users.noreply.github.com>
2022-01-10 15:56:33 +0100

    Add files via upload

---
+++ Login, Passwort.py
@@ -0,0 +1,9 @@
+# Dieses Programm fragt nach ihrem Passwort, das sie korekkt eingebe muss.
+# Es wird nach ihrem Login gefragt
+x = input("Login eingeben: ")
+# wenn ihr Login "Strudel123" ist, sagt der Code dir "Herzlich Willkommen"
+if x == "Strudel123":
+    print("Herzlich Willkommen")
+# wenn das Login nicht "Strudel123" ist, sagt der Code dir "Passwort nicht korrekt"
+else: 
+    print("Passwort nicht korrekt")
