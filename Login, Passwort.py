2e2ee18df92235246629a2518878a393628740c8
Maria222222 <97455672+Maria222222@users.noreply.github.com>
2022-01-10 16:12:46 +0100

    Update Login, Passwort.py

--- Login, Passwort.py
+++ Login, Passwort.py
@@ -1,3 +1,9 @@
+# Einleitung: In diesem Program können Sie Ihr Passwort eingeben und es für Sie überprüfen, ob es richtig ist. Wenn Sie das korrekte Passwort eingegeben haben, bergrüsst es Sie mit "Herzlich Willkommen". Ist es jedoch das falsche Passwort, gibt das Program als Rückmeldung: "Passwort nicht korrekt". Sie können das Passwort aber natürlich nach Belieben verändern.
+# Feature: 
+
+# Revlexion: Ich habe gelernt, dass Programmiersprachen viel komplexer aufgebaut sind, als ich mir vorgestelt habe. Ich habe gelernt, dass man praktisch alles Programmieren kann. Über Einkaufslisten, zu Logins bis zu den Notendurchschnittberecher. 
+
+
 # Dieses Programm fragt nach ihrem Passwort, das sie korekkt eingebe muss.
 # Es wird nach ihrem Login gefragt
 x = input("Login eingeben: ")
